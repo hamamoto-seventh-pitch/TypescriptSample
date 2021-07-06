53e62eadbe8d007fb617df0c9d582da229f55e5d
cbaseMasaoHamamoto <masao_hamamoto@cbase.com>
2021-07-07 03:17:00 +0900

    mod CI

--- src/world.ts
+++ src/world.ts
@@ -5,24 +5,9 @@ export default class World {
     this.message = message
   }
 
-
-
-
-
-
-
-
   public sayHallo(elem: HTMLElement | null): void {
     if (elem) {
       elem.innerHTML = this.message
-
-
-
-
-
-
-
-      
     }
   }
 }
